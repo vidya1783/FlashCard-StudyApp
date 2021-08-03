3ead46bf658b7458cf117a72f35204b5426a7e4b
Joseph Mirarchi <josephdm@gmail.com>
2021-08-03 11:11:49 -0400

    create deck, create flashcard, view decks now all use authentication

--- java/src/main/java/com/techelevator/controller/FlashcardController.java
+++ java/src/main/java/com/techelevator/controller/FlashcardController.java
@@ -1,34 +1,38 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.FlashcardDao;
+import com.techelevator.dao.UserDao;
 import com.techelevator.model.Flashcard;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
+@PreAuthorize("isAuthenticated()")
 public class FlashcardController {
 
     private FlashcardDao flashcardDao;
+    private UserDao userDao;
 
-    public FlashcardController(FlashcardDao flashcardDao)
+    public FlashcardController(FlashcardDao flashcardDao, UserDao userDao)
     {
         this.flashcardDao = flashcardDao;
+        this.userDao = userDao;
     }
 
     /**
      * @return the ID of a newly created card
      * **/
     @RequestMapping(path="flashcard", method= RequestMethod.POST)
-    public Flashcard createFlashcard(@RequestBody Flashcard flashcard) throws Exception
+    public Flashcard createFlashcard(@RequestBody Flashcard flashcard, Principal principal) throws Exception
     {
         Flashcard newFlashcard;
-
-        Long id = flashcard.getCreatorId();
-        String questionText = flashcard.getQuestionText();
-        String answerText = flashcard.getAnswerText();
-
+        Long userId = Long.valueOf(userDao.findIdByUsername(principal.getName()));
+        flashcard.setCreatorId(userId);
 
         try {
             newFlashcard = flashcardDao.createTextFlashcard(flashcard.getCreatorId(),
