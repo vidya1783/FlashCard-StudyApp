eca41fe93a604f6cc2988c8c5b1a24cb9edbcb80
Carlos <solrac.kruger@gmail.com>
2021-08-05 12:19:43 -0400

    DeckDetails

--- vue/src/components/DeckDetails.vue
+++ vue/src/components/DeckDetails.vue
@@ -1,10 +1,52 @@
 <template>
-  <h1>This is the test page</h1>
+<div >  <h1>We are now inside the component</h1>
+{{ deck.deck_name }}
+this components' own data 
+{{detailsData.deck_description}}
+   <v-form>
+       <v-container>
+           <v-layout>
+               <v-flex xs12 md4>
+                   <v-text-field v-model="detailsData.deck_name"  :counter="70" label="Deck Name" required>
+                   </v-text-field>
+               </v-flex>
+               <v-flex xs12 md4>
+                   <v-text-field v-model="detailsData.deck_description" :counter="70" label="Deck Description" required>
+                   </v-text-field>
+               </v-flex>
+               <v-flex xs12 md4>
+                   <v-text-field ></v-text-field>
+               </v-flex>
+           </v-layout>
+       </v-container>
+   </v-form>
+
+
+
+
+
+
+</div>
+
+
+
 </template>
 
 <script>
 
+
 export default {
+  name: 'deck-details',
+  props: ['deck'],
+  data(){
+    return {
+      detailsData: {deck_name: "defaultName", deck_description: "default deck description" } 
+    }
+  },
+  created() {
+    this.detailsData.deck_name = this.deck.deck_name;
+    this.detailsData.deck_description = this.deck.deck_description;
+  },
 
 }
 </script>
--- vue/src/views/TestDeckDetails.vue
+++ vue/src/views/TestDeckDetails.vue
@@ -1,13 +1,25 @@
 <template>
-  <deck-details/>
+<div > 
+  This before we call the deck-details element
+  {{ deck.deck_name }} {{ deck.deck_description}}
+  <deck-details  v-bind:deck="deck" />
+  </div> 
 </template>
 
 <script>
 import deckDetails from '../components/DeckDetails.vue';
 
 export default {
-    name: 'deck-details',
-    components: {
+    name: 'test-deck-details',
+ data(){
+   return {
+     deck: {
+       deck_name: 'Carlos Deck',
+       deck_description: 'the best deck ever'
+     }
+   }
+ },
+ components: {
         deckDetails
     }
 
