df94606e1c15c8f2f420a36fd52812f33e3d2be0
Ash Williams <83557250+gdash-code@users.noreply.github.com>
2021-08-06 13:19:14 -0400

    working log in

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,7 +1,45 @@
 <template>
-  <div id="login" class="text-center">
-    <form class="form-signin" @submit.prevent="login">
+
+  <div id="login" class="text-center" align="center">
+    
+    <v-form class="form-signin" @submit.prevent="login" >
       <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
+      <router-link :to="{ name: 'register' }">Need an account?</router-link>
+      <v-spacer></v-spacer>
+      <div align="center">
+      <v-container fill-height fluid>
+      <v-layout row align-center>
+      <v-flex >
+      <v-text-field 
+        filled
+        rounded
+        dense
+        type="text"
+        id="username"
+        class="form-control"
+        placeholder="Username"
+        v-model="user.username"
+        required
+        autofocus
+      />
+      
+      <v-text-field
+      filled
+      rounded
+        dense
+        type="password"
+        id="password"
+        class="form-control"
+        placeholder="Password"
+        v-model="user.password"
+        required
+      />
+      </v-flex>
+      </v-layout>
+      </v-container>
+      </div>
+      
+      </v-form>
       <div
         class="alert alert-danger"
         role="alert"
@@ -12,29 +50,10 @@
         role="alert"
         v-if="this.$route.query.registration"
       >Thank you for registering, please sign in.</div>
-      <label for="username" class="sr-only">Username</label>
-      <input
-        type="text"
-        id="username"
-        class="form-control"
-        placeholder="Username"
-        v-model="user.username"
-        required
-        autofocus
-      />
-      <label for="password" class="sr-only">Password</label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
-      <router-link :to="{ name: 'register' }">Need an account?</router-link>
-      <v-btn type="submit">Sign in</v-btn>
+
       
-    </form>
+      
+      <v-btn type="submit">Sign in</v-btn>
   </div>
 </template>
 
@@ -76,5 +95,10 @@ export default {
 };
 </script>
  
-
+<style>
+div #username{
+  display: flex;
+  align-items:center;
+}
+</style>
       
