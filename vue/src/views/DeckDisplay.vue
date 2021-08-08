2f48e13f6ce31f1ed3b493e6810f92961dd0304b
Carlos <solrac.kruger@gmail.com>
2021-08-08 16:18:44 -0400

    DeckDisplay updated to display all cards associated with a particular deck

--- vue/src/views/DeckDisplay.vue
+++ vue/src/views/DeckDisplay.vue
@@ -1,14 +1,43 @@
 <template>
   <div>
-      This is the deck display
+      This is the deck display for deck: {{$route.params.id}}
+
+        <card-details v-for="flashcard in flashcards"
+  v-bind:flashcard="flashcard" v-bind:key="flashcard.id" />
+  <card-details />
+  <button>Study Session</button>
   </div>
 </template>
 
 <script>
-export default {
+import cardDetails from '../components/CardDetails.vue';
+import deckService from '../services/DeckService.js'
 
-}
+export default {
+    
+    name: 'deck-display',
+    data(){
+        return {
+             flashcards: []
+        }  
+    },
+    created(){
+        console.log("greetings from created")
+        deckService.getFlashcardsByDeckId(this.$route.params.id)
+         .then((response) => { 
+            this.flashcards = response.data;
+        });
+    },
+    components: {
+        cardDetails
+    }
+    }
+
+
+
+
 </script>
+
 
 <style>
 
